934da3858a274ff85fd6765c15f5fed60aca2478
tetradev1 <9chakri+ds@gmail.com>
2017-09-07 20:16:18 -0700

    update

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 require 'webrick'
+json = File.read("./products.json")
+class Server < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, json)
+    super(server)
+    @json = json
+  end
 
-#FileHandler servlet provides the option to choose which files from user to serve
-#The following code shows how to serve them from the folder 'myapp'
-root = File.expand_path '/var/myapp/'
-
-#Instantiating a new server with HTTPServer.new on port 1234 serving the documents from root folder
-server = WEBrick::HTTPServer.new :Port => 1234, :DocumentRoot => root
-
-#The following proc is used to customize the server operations
-server.mount_proc '/' do |request, response|
-  response.body = 'Hello, world!'
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'application/text'
+    response.body = @json
+  end
 end
 
-#The following command will provide a hook to shutdown the server (often done with Ctrl+C)
-trap('INT') {server.shutdown}
+server = WEBrick::HTTPServer.new :Port => 8080
+server.mount "/", Server, json
 
-#Start the server
+trap 'INT' do server.shutdown end
 server.start
